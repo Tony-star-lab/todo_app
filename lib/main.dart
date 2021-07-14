1824fbfdcae78d16fb6f012a34e8c5cab2658578
Tony-star-lab <gelastonny@gmail.com>
2021-07-14 03:06:07 +0300

    Updated main title

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tony ToDo',
+      title: 'Tony Todo App',
       theme: ThemeData(
         // This is the theme of your application.
         //
